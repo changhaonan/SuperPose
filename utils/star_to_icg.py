442166db09be9d6e52556ce95956c49ad834ad97
changhaonan <chnme40cs@gmail.com>
2023-02-03 12:46:02 -0500

    update: prepare the scripts

--- utils/star_to_icg.py
+++ utils/star_to_icg.py
@@ -9,6 +9,8 @@ import os
 import numpy as np
 import yaml
 import json
+import open3d as o3d
+import cv2
 
 def np_mat_to_yaml(mat):
     yaml_mat = {}
@@ -20,118 +22,155 @@ def np_mat_to_yaml(mat):
 
 
 def generate_icg_tracker(tracker_name, data_dir, icg_dir):
-    # copy the obj file
+    # generate the obj file if not exist
     obj_path = os.path.join(data_dir, "star", "reconstruct.obj")
+    if not os.path.exists(obj_path):
+        print("Generating the obj file...")
+        pcd_path = os.path.join(data_dir, "star", "reconstruct.pcd")
+        pcd = o3d.io.read_point_cloud(pcd_path)
+        # transfer point cloud to obj using possion
+        mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
+        o3d.io.write_triangle_mesh(obj_path, mesh)
+    
+    # copy the obj file
     icg_obj_path = os.path.join(icg_dir, "reconstruct.obj")
     os.makedirs(os.path.dirname(icg_obj_path), exist_ok=True)
     os.system("cp {} {}".format(obj_path, icg_obj_path))
-    # prepare the camera color yaml
+
+    # save the config yaml
+    config_yaml_path = os.path.join(icg_dir, "config.yaml")
+    config_s = cv2.FileStorage(config_yaml_path, cv2.FileStorage_WRITE)
+
+    # save the camera yaml
+    config_s.startWriteStruct("LoaderColorCamera", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "loader_color")
+    config_s.write("metafile_path", "camera_color.yaml")
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+
+    # save the normal color viewer
+    config_s.startWriteStruct("NormalColorViewer", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "color_viewer")
+    config_s.write("color_camera", "loader_color")
+    config_s.write("renderer_geometry", "renderer_geometry")
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+
+    # save the renderer geometry
+    config_s.startWriteStruct("RendererGeometry", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "renderer_geometry")
+    config_s.write("bodies", [tracker_name])
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+
+    # save the body
+    config_s.startWriteStruct("Body", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", tracker_name)
+    config_s.write("metafile_path", "object.yaml")
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+
+    # save the static detector
+    config_s.startWriteStruct("StaticDetector", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "static_detector")
+    config_s.write("metafile_path", "detector.yaml")
+    config_s.write("body", tracker_name)
+    config_s.write("color_camera", "loader_color")
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+
+    # save the region model
+    config_s.startWriteStruct("RegionModel", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "region_model")
+    config_s.write("metafile_path", "model.yaml")
+    config_s.write("body", tracker_name)
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+
+    # save the region modality
+    config_s.startWriteStruct("RegionModality", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "region_modality")
+    config_s.write("body", tracker_name)
+    config_s.write("color_camera", "loader_color")
+    config_s.write("region_model", "region_model")
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+
+    # save the optimizer
+    config_s.startWriteStruct("Optimizer", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "optimizer")
+    config_s.write("modalities", "region_modality")
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+    
+    # save the tracker
+    config_s.startWriteStruct("Tracker", cv2.FileNode_SEQ)
+    config_s.startWriteStruct("", cv2.FileNode_MAP)
+    config_s.write("name", "tracker")
+    config_s.write("viewers", ["color_viewer"])
+    config_s.write("detectors", ["static_detector"])
+    config_s.write("optimizers", ["optimizer"])
+    config_s.endWriteStruct()
+    config_s.endWriteStruct()
+    config_s.release()
+
+    # save the cam color
     context_json_file = os.path.join(data_dir, "star", "context.json")
     with open(context_json_file, "r") as f:
         context_data = json.load(f)
-    
-    # prepare the camera color yaml
-    cam_color_yaml = {}
-    cam_color_yaml["load_directory"] = os.path.join(data_dir, "color")
-    cam_color_yaml["intrinsics"] = {}
-    cam_color_yaml["intrinsics"]["f_u"] = context_data["cam-00"]["intrinsic"][0]
-    cam_color_yaml["intrinsics"]["f_v"] = context_data["cam-00"]["intrinsic"][1]
-    cam_color_yaml["intrinsics"]["pp_x"] = context_data["cam-00"]["intrinsic"][2]
-    cam_color_yaml["intrinsics"]["pp_y"] = context_data["cam-00"]["intrinsic"][3]
-    cam_color_yaml["intrinsics"]["width"] = context_data["cam-00"]["image_cols"]
-    cam_color_yaml["intrinsics"]["height"] = context_data["cam-00"]["image_rows"]
-    cam_color_yaml["camera2world_pose"] = np_mat_to_yaml(context_data["cam-00"]["extrinsic"])
-    cam_color_yaml["depth_scale"] = 1.0
-    cam_color_yaml["image_name_pre"] = ""
-    cam_color_yaml["load_index"] = 0
-    cam_color_yaml["n_leading_zeros"] = 0
-    cam_color_yaml["image_name_post"] = ""
-    cam_color_yaml["load_image_type"] = "png"
-    # save the yaml
-    cam_color_yaml_path = os.path.join(icg_dir, "camera_color.yaml")
-    with open(cam_color_yaml_path, "w") as f:
-        yaml.dump(cam_color_yaml, f)
 
-    # prepare the detector yaml (Manual detector set the pose in the first frame)
+    config_yaml_path = os.path.join(icg_dir, "camera_color.yaml")
+    cam_color_s = cv2.FileStorage(config_yaml_path, cv2.FileStorage_WRITE)
+    cam_color_s.write("load_directory", os.path.join(data_dir, "color"))
+    cam_color_s.startWriteStruct("intrinsics", cv2.FileNode_MAP)
+    cam_color_s.write("f_u", context_data["cam-00"]["intrinsic"][0])
+    cam_color_s.write("f_v", context_data["cam-00"]["intrinsic"][1])
+    cam_color_s.write("pp_x", context_data["cam-00"]["intrinsic"][2])
+    cam_color_s.write("pp_v", context_data["cam-00"]["intrinsic"][3])
+    cam_color_s.write("width", context_data["cam-00"]["image_cols"])
+    cam_color_s.write("height", context_data["cam-00"]["image_rows"])
+    cam_color_s.endWriteStruct()
+    extrinsic = np.array(context_data["cam-00"]["extrinsic"]).reshape(4, 4)
+    cam_color_s.write("camara2world_pose", extrinsic)
+    cam_color_s.write("depth_scale", 1.0)
+    cam_color_s.write("image_name_pre", "")
+    cam_color_s.write("load_index", 0)
+    cam_color_s.write("n_leading_zeros", 0)
+    cam_color_s.write("image_name_post", "")
+    cam_color_s.write("load_image_type", "png")
+    cam_color_s.release()
+
+    # save the detector
     kf_results = np.load(os.path.join(args.data_dir, "star", "kf_results.npz"))
     cam_poses = kf_results["cam_poses"]
-    detector_yaml = {}
-    detector_yaml["body2world_pose"] = np_mat_to_yaml(cam_poses[0])
-    # save the yaml
-    detector_yaml_path = os.path.join(icg_dir, "detector.yaml")
-    with open(detector_yaml_path, "w") as f:
-        yaml.dump(detector_yaml, f)
+    config_yaml_path = os.path.join(icg_dir, "detector.yaml")
+    detector_s = cv2.FileStorage(config_yaml_path, cv2.FileStorage_WRITE)
+    detector_s.write("body2world_pose", cam_poses[0])
+    detector_s.release()
 
-    # prepare the object yaml
-    obj_yaml = {}
-    obj_yaml["geometry_path"] = "reconstruct.obj"
-    obj_yaml["geometry_unit_in_meters"] = 1.0  # scale
-    obj_yaml["geometry_counterclockwise"] = 1
-    obj_yaml["geometry_enable_culling"] = 1
-    obj_yaml["geometry2body_pose"] = np_mat_to_yaml(np.eye(4))
-    # save the yaml
-    obj_yaml_path = os.path.join(icg_dir, "object.yaml")
-    with open(obj_yaml_path, "w") as f:
-        yaml.dump(obj_yaml, f)
-    
-    # default model yaml
-    model_yaml = {}
-    model_yaml["model_path"] = "INFER_FROM_NAME"
-    # save the yaml
-    model_yaml_path = os.path.join(icg_dir, "model.yaml")
-    with open(model_yaml_path, "w") as f:
-        yaml.dump(model_yaml, f)
-    
-    # finally, the config yaml
-    config_yaml = {}
-    config_yaml["LoaderColorCamera"] = {}
-    config_yaml["LoaderColorCamera"]["name"] = "loader_color"
-    config_yaml["LoaderColorCamera"]["metafile_path"] = "camera_color.yaml"
+    # save the model
+    config_yaml_path = os.path.join(icg_dir, "model.yaml")
+    model_s = cv2.FileStorage(config_yaml_path, cv2.FileStorage_WRITE)
+    model_s.write("model_path", "INFER_FROM_NAME")
+    model_s.release()
 
-    config_yaml["NormalColorViewer"] = {}
-    config_yaml["NormalColorViewer"]["name"] = "color_viewer"
-    config_yaml["NormalColorViewer"]["color_camera"] = "loader_color"
-    config_yaml["NormalColorViewer"]["renderer_geometry"] = "renderer_geometry"
-
-    config_yaml["RendererGeometry"] = {}
-    config_yaml["RendererGeometry"]["name"] = "renderer_geometry"
-    config_yaml["RendererGeometry"]["bodies"] = [tracker_name]
-
-    config_yaml["Body"] = {}
-    config_yaml["Body"]["name"] = tracker_name
-    config_yaml["Body"]["metafile_path"] = "object.yaml"
-
-    config_yaml["StaticDetector"] = {}
-    config_yaml["StaticDetector"]["name"] = "static_detector"
-    config_yaml["StaticDetector"]["metafile_path"] = "detector.yaml"
-    config_yaml["body"] = tracker_name
-    config_yaml["color_camera"] = "loader_color"
-
-    config_yaml["RegionModel"] = {}
-    config_yaml["RegionModel"]["name"] = "region_model"
-    config_yaml["RegionModel"]["metafile_path"] = "model.yaml"
-    config_yaml["body"] = tracker_name
-
-    config_yaml["RegionModality"] = {}
-    config_yaml["RegionModality"]["name"] = "region_modality"
-    config_yaml["RegionModality"]["body"] = tracker_name
-    config_yaml["RegionModality"]["color_camera"] = "loader_color"
-    config_yaml["RegionModality"]["region_model"] = "region_model"
-
-    config_yaml["Optimizer"] = {}
-    config_yaml["Optimizer"]["name"] = "optimizer"
-    config_yaml["Optimizer"]["modalities"] = "region_modality"
-
-    config_yaml["Tracker"] = {}
-    config_yaml["Tracker"]["name"] = "tracker"
-    config_yaml["Tracker"]["viewers"] = ["color_viewer"]
-    config_yaml["Tracker"]["detectors"] = ["static_detector"]
-    config_yaml["Tracker"]["optimizers"] = ["optimizer"]
-
-    # save the yaml
-    config_yaml_path = os.path.join(icg_dir, "config.yaml")
-    with open(config_yaml_path, "w") as f:
-        yaml.dump(config_yaml, f)
+    # save the object
+    config_yaml_path = os.path.join(icg_dir, "object.yaml")
+    object_s = cv2.FileStorage(config_yaml_path, cv2.FileStorage_WRITE)
+    object_s.write("geometry_path", "reconstruct.obj")
+    object_s.write("geometry_unit_in_meter", 1.0)
+    object_s.write("geometry_counterclockwise", 1)
+    object_s.write("geometry_enable_culling", 1)
+    object_s.write("geometry2body_pose", np.eye(4))
+    object_s.release()
 
 
 if __name__ == "__main__":
