c7b0de88afab20a74f73bbe88cf31ecdb890e82f
changhaonan <chnme40cs@gmail.com>
2023-02-10 22:42:20 -0500

    update: still need to check superpoints.

--- cv_server/run_orb_server.py
+++ cv_server/run_orb_server.py
@@ -1,20 +1,18 @@
-# Copyright 2019-present NAVER Corp.
-# CC BY-NC-SA 3.0
-# Available only for non-commercial use
-
 from __future__ import print_function
 import numpy as np
 import zmq
 import cv2
 from tqdm import tqdm
 import numpy as np
+import os
 
 
 class ORBExtractor:
-    def __init__(self, args):
-        self.top_k = args.top_k
+    def __init__(self, top_k, save_dir):
+        self.top_k = top_k
+        self.save_dir = save_dir
 
-    def extract(self, img):
+    def extract(self, img, frame_idx):
         # extract keypoints/descriptors for a single image
         orb = cv2.ORB_create()
         kpts, desc = orb.detectAndCompute(img, None)
@@ -22,11 +20,16 @@ class ORBExtractor:
         scores = np.array([k.response for k in kpts])
         idxs = scores.argsort()[-self.top_k or None :]
         # visualize
-        img = cv2.drawKeypoints(img, kpts, None, color=(0, 255, 0), flags=0)
-        cv2.imshow('img', img)
-        cv2.waitKey(1)
-        if len(idxs) == 0:
-            return np.zeros((0, 2)), np.zeros((0, 32)), np.zeros((0, ))
+        # img = cv2.drawKeypoints(img, kpts, None, color=(0, 255, 0), flags=0)
+        # cv2.imshow('img', img)
+        # cv2.waitKey(1)
+        # if len(idxs) == 0:
+        #     return np.zeros((0, 2)), np.zeros((0, 32)), np.zeros((0, ))
+        # save the image
+        if self.save_dir is not None:
+            if os.path.exists(self.save_dir) is False:
+                os.makedirs(self.save_dir)
+            cv2.imwrite(os.path.join(self.save_dir, f"{frame_idx}.png"), img)
         return xys[idxs], desc[idxs], scores[idxs]
 
 
@@ -34,20 +37,21 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tag", type=str, default='r2d2', help='output file tag')
-    
-    parser.add_argument("--top-k", type=int, default=5000, help='number of keypoints')
-
+    parser.add_argument("--tag", type=str, default="r2d2", help="output file tag")
+    parser.add_argument("--save-dir", type=str, default=None, help="data directory")
+    parser.add_argument("--top-k", type=int, default=5000, help="number of keypoints")
     parser.add_argument("--scale-f", type=float, default=2**0.25)
     parser.add_argument("--min-size", type=int, default=256)
     parser.add_argument("--max-size", type=int, default=1024)
     parser.add_argument("--min-scale", type=float, default=0)
     parser.add_argument("--max-scale", type=float, default=1)
-    
+
     parser.add_argument("--reliability-thr", type=float, default=0.7)
     parser.add_argument("--repeatability-thr", type=float, default=0.7)
 
-    parser.add_argument("--gpu", type=int, nargs='+', default=[0], help='use -1 for CPU')
+    parser.add_argument(
+        "--gpu", type=int, nargs="+", default=[0], help="use -1 for CPU"
+    )
     parser.add_argument("--port", type=int, default=5555)
     args = parser.parse_args()
 
@@ -58,8 +62,8 @@ if __name__ == "__main__":
     socket.bind(port)
 
     # init extractor
-    extractor = ORBExtractor(args)
-
+    extractor = ORBExtractor(args.top_k, args.save_dir)
+    frame_idx = 0
     while True:
         print(f"ORB listending to {port}")
         msgs = socket.recv_multipart(0)
@@ -72,7 +76,7 @@ if __name__ == "__main__":
         image = np.frombuffer(msg, dtype=np.uint8).reshape(H, W, -1).squeeze()
 
         # extract keypoints/descriptors
-        xys, desc, scores = extractor.extract(image)
+        xys, desc, scores = extractor.extract(image, frame_idx)
 
         num_feat = len(xys)
         feat_dim = desc.shape[1]
@@ -87,3 +91,6 @@ if __name__ == "__main__":
         socket.send(msg, 2)
         msg = desc.astype(np.float32).reshape(-1).tobytes()
         socket.send(msg, 0)
+
+        # update frame index
+        frame_idx += 1
--- scripts/run_feature_server.sh
+++ scripts/run_feature_server.sh
@@ -1,16 +1,18 @@
 # set path
 export SUPERPOSE_PATH="/home/robot-learning/Projects/SuperPose"
-export FEATURE_TYPR="orb"
+export FEATURE_TYPR="r2d2"
+export EXERIMENT_NAME="Scene"
 
 # switch on feature type
 if [ ${FEATURE_TYPR} = "r2d2" ]; then
     # change workspace
     cd ${SUPERPOSE_PATH}/r2d2
     # run feature server
-    python ${SUPERPOSE_PATH}/r2d2/run_server.py --port=9090 --model=models/r2d2_WASF_N16.pt --top-k=500
+    python ${SUPERPOSE_PATH}/r2d2/run_r2d2_server.py --port=9090 --model=models/r2d2_WASF_N16.pt --top-k=500
 elif [ ${FEATURE_TYPR} = "orb" ]; then
      # change workspace
     cd ${SUPERPOSE_PATH}/cv_server
     # run feature server
-    python ${SUPERPOSE_PATH}/cv_server/run_server.py --port=9090 --top-k=500
+    python ${SUPERPOSE_PATH}/cv_server/run_orb_server.py --port=9090 --top-k=500
+    # python ${SUPERPOSE_PATH}/cv_server/run_orb_server.py --port=9090 --top-k=500 --save-dir=${SUPERPOSE_PATH}/debug/${EXERIMENT_NAME}
 fi
