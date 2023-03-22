7fa0ae53808b3353e1798bfccccd6732dd910369
changhaonan <chnme40cs@gmail.com>
2023-03-21 20:43:05 -0400

    update: fix the sfm model

--- one_pose_sfm.py
+++ one_pose_sfm.py
@@ -10,93 +10,87 @@ from pathlib import Path
 from omegaconf import DictConfig
 
 
-def merge_(anno_2d_file, avg_anno_3d_file, collect_anno_3d_file,
-           idxs_file, img_id, ann_id, images, annotations):
-    """ To prepare training and test objects, we merge annotations about difference objs"""
-    with open(anno_2d_file, 'r') as f:
+def merge_(anno_2d_file, avg_anno_3d_file, collect_anno_3d_file, idxs_file, img_id, ann_id, images, annotations):
+    """To prepare training and test objects, we merge annotations about difference objs"""
+    with open(anno_2d_file, "r") as f:
         annos_2d = json.load(f)
-    
+
     for anno_2d in annos_2d:
         img_id += 1
         info = {
-            'id': img_id,
-            'img_file': anno_2d['img_file'],
+            "id": img_id,
+            "img_file": anno_2d["img_file"],
         }
         images.append(info)
 
         ann_id += 1
         anno = {
-            'image_id': img_id,
-            'id': ann_id,
-            'pose_file': anno_2d['pose_file'],
-            'anno2d_file': anno_2d['anno_file'],
-            'avg_anno3d_file': avg_anno_3d_file,
-            'collect_anno3d_file': collect_anno_3d_file,
-            'idxs_file': idxs_file
+            "image_id": img_id,
+            "id": ann_id,
+            "pose_file": anno_2d["pose_file"],
+            "anno2d_file": anno_2d["anno_file"],
+            "avg_anno3d_file": avg_anno_3d_file,
+            "collect_anno3d_file": collect_anno_3d_file,
+            "idxs_file": idxs_file,
         }
         annotations.append(anno)
     return img_id, ann_id
 
 
 def merge_anno(cfg):
-    """ Merge different objects' anno file into one anno file """
+    """Merge different objects' anno file into one anno file"""
     anno_dirs = []
 
-    if cfg.split == 'train':
+    if cfg.split == "train":
         names = cfg.train.names
-    elif cfg.split == 'val':
+    elif cfg.split == "val":
         names = cfg.val.names
-    
+
     for name in names:
-        anno_dir = osp.join(cfg.datamodule.data_dir, name, f'outputs_{cfg.network.detection}_{cfg.network.matching}', 'anno')
-        anno_dirs.append(anno_dir) 
-    
+        anno_dir = osp.join(cfg.datamodule.data_dir, name, f"outputs_{cfg.network.detection}_{cfg.network.matching}", "anno")
+        anno_dirs.append(anno_dir)
+
     img_id = 0
     ann_id = 0
     images = []
     annotations = []
     for anno_dir in anno_dirs:
-        logger.info(f'Merging anno dir: {anno_dir}')
-        anno_2d_file = osp.join(anno_dir, 'anno_2d.json')
-        avg_anno_3d_file = osp.join(anno_dir, 'anno_3d_average.npz')
-        collect_anno_3d_file = osp.join(anno_dir, 'anno_3d_collect.npz')
-        idxs_file = osp.join(anno_dir, 'idxs.npy')
+        logger.info(f"Merging anno dir: {anno_dir}")
+        anno_2d_file = osp.join(anno_dir, "anno_2d.json")
+        avg_anno_3d_file = osp.join(anno_dir, "anno_3d_average.npz")
+        collect_anno_3d_file = osp.join(anno_dir, "anno_3d_collect.npz")
+        idxs_file = osp.join(anno_dir, "idxs.npy")
 
         if not osp.isfile(anno_2d_file) or not osp.isfile(avg_anno_3d_file) or not osp.isfile(collect_anno_3d_file):
-            logger.info(f'No annotation in: {anno_dir}')
+            logger.info(f"No annotation in: {anno_dir}")
             continue
-        
-        img_id, ann_id = merge_(anno_2d_file, avg_anno_3d_file, collect_anno_3d_file,
-                                idxs_file, img_id, ann_id, images, annotations)
-    
-    logger.info(f'Total num: {len(images)}')
-    instance = {'images': images, 'annotations': annotations}
+
+        img_id, ann_id = merge_(anno_2d_file, avg_anno_3d_file, collect_anno_3d_file, idxs_file, img_id, ann_id, images, annotations)
+
+    logger.info(f"Total num: {len(images)}")
+    instance = {"images": images, "annotations": annotations}
 
     out_dir = osp.dirname(cfg.datamodule.out_path)
     Path(out_dir).mkdir(exist_ok=True, parents=True)
-    with open(cfg.datamodule.out_path, 'w') as f:
+    with open(cfg.datamodule.out_path, "w") as f:
         json.dump(instance, f)
 
 
 def sfm(cfg):
-    """ Reconstruct and postprocess sparse object point cloud, and store point cloud features"""
+    """Reconstruct and postprocess sparse object point cloud, and store point cloud features"""
     from one_pose.utils import path_utils
 
     data_dirs = cfg.dataset.data_dir
     down_ratio = cfg.sfm.down_ratio
     data_dirs = [data_dirs] if isinstance(data_dirs, str) else data_dirs
-    
+
     for data_dir in data_dirs:
         logger.info(f"Processing {data_dir}.")
-        root_dir, sub_dirs = data_dir.split(' ')[0], data_dir.split(' ')[1:]
-
         # Parse image directory and downsample images:
         img_lists = []
         seg_lists = []
-        for sub_dir in sub_dirs:
-            seq_dir = osp.join(root_dir, sub_dir)
-            img_lists += glob.glob(str(Path(seq_dir)) + '/color/*.png', recursive=True)
-            seg_lists += glob.glob(str(Path(seq_dir)) + '/seg/*.png', recursive=True)
+        img_lists += glob.glob(str(Path(data_dir)) + "/color/*.png", recursive=True)
+        seg_lists += glob.glob(str(Path(data_dir)) + "/seg/*.png", recursive=True)
 
         # Filter out images based on the existence of pose file:
         pose_lists = [path_utils.get_gt_pose_path_by_color(color_path) for color_path in img_lists]
@@ -104,45 +98,47 @@ def sfm(cfg):
         seg_lists = [seg for seg, pose in zip(seg_lists, pose_lists) if osp.isfile(pose)]
 
         # Sorted img_lists:
-        img_lists = sorted(img_lists, key=lambda x: int(x.split('/')[-1].split('.')[0]))
-        seg_lists = sorted(seg_lists, key=lambda x: int(x.split('/')[-1].split('.')[0]))
+        img_lists = sorted(img_lists, key=lambda x: int(x.split("/")[-1].split(".")[0]))
+        seg_lists = sorted(seg_lists, key=lambda x: int(x.split("/")[-1].split(".")[0]))
         down_img_lists = []
         down_seg_lists = []
-        for seg_file, img_file in zip(seg_lists, img_lists):
-            index = int(img_file.split('/')[-1].split('.')[0])
+        for i in range(len(img_lists)):
+            img_file = img_lists[i]
+            index = int(img_file.split("/")[-1].split(".")[0])
             if index % down_ratio == 0:
-                down_img_lists.append(img_file)  
-                down_seg_lists.append(seg_file)
+                down_img_lists.append(img_file)
+                if len(seg_lists) > 0:
+                    seg_file = seg_lists[i]
+                    down_seg_lists.append(seg_file)
 
         img_lists = down_img_lists
         seg_lists = down_seg_lists
-        if len(img_lists) == 0 or len(seg_lists) == 0:
-            logger.info(f"No png image in {root_dir}")
+        if len(img_lists) == 0:
+            logger.info(f"No png image in {data_dir}")
             continue
-        
-        obj_name = root_dir.split('/')[-1]
+
+        obj_name = data_dir.split("/")[-1]
         outputs_dir_root = cfg.dataset.outputs_dir.format(obj_name)
 
         # Begin SfM and postprocess:
         sfm_core(cfg, img_lists, seg_lists, outputs_dir_root)
-        postprocess(cfg, img_lists, root_dir, outputs_dir_root) 
+        postprocess(cfg, img_lists, data_dir, outputs_dir_root)
 
 
 def sfm_core(cfg, img_lists, seg_lists, outputs_dir_root):
-    """ Sparse reconstruction: extract features, match features, triangulation"""
-    from one_pose.sfm import extract_features_ext, match_features, \
-                         generate_empty, triangulation, pairs_from_poses
+    """Sparse reconstruction: extract features, match features, triangulation"""
+    from one_pose.sfm import extract_features_ext, match_features, generate_empty, triangulation, pairs_from_poses
 
     # Construct output directory structure:
-    outputs_dir = osp.join(outputs_dir_root, 'outputs' + '_' + cfg.network.detection + '_' + cfg.network.matching)
-    feature_out = osp.join(outputs_dir, f'feats-{cfg.network.detection}.h5')
-    covis_pairs_out = osp.join(outputs_dir, f'pairs-covis{cfg.sfm.covis_num}.txt')
-    matches_out = osp.join(outputs_dir, f'matches-{cfg.network.matching}.h5')
-    empty_dir = osp.join(outputs_dir, 'sfm_empty')
-    deep_sfm_dir = osp.join(outputs_dir, 'sfm_ws')
-    
+    outputs_dir = osp.join(outputs_dir_root, "outputs" + "_" + cfg.network.detection + "_" + cfg.network.matching)
+    feature_out = osp.join(outputs_dir, f"feats-{cfg.network.detection}.h5")
+    covis_pairs_out = osp.join(outputs_dir, f"pairs-covis{cfg.sfm.covis_num}.txt")
+    matches_out = osp.join(outputs_dir, f"matches-{cfg.network.matching}.h5")
+    empty_dir = osp.join(outputs_dir, "sfm_empty")
+    deep_sfm_dir = osp.join(outputs_dir, "sfm_ws")
+
     if cfg.redo:
-        os.system(f'rm -rf {outputs_dir}') 
+        os.system(f"rm -rf {outputs_dir}")
         Path(outputs_dir).mkdir(exist_ok=True, parents=True)
 
         # Extract image features, construct image pairs and then match:
@@ -154,35 +150,35 @@ def sfm_core(cfg, img_lists, seg_lists, outputs_dir_root):
         generate_empty.generate_model(img_lists, empty_dir)
         triangulation.main(deep_sfm_dir, empty_dir, outputs_dir, covis_pairs_out, feature_out, matches_out, image_dir=None)
 
-    
+
 def postprocess(cfg, img_lists, root_dir, outputs_dir_root):
-    """ Filter points and average feature"""
+    """Filter points and average feature"""
     from one_pose.sfm.postprocess import filter_points, feature_process, filter_tkl
-    from one_pose.utils.colmap import read_write_model 
+    from one_pose.utils.colmap import read_write_model
     from one_pose.utils.geo_utils import points_colmap_to_o3d, bbox_o3d_to_onepose
     import open3d as o3d
 
     bbox_path = osp.join(root_dir, "box3d_corners.txt")
     # Construct output directory structure:
-    outputs_dir = osp.join(outputs_dir_root, 'outputs' + '_' + cfg.network.detection + '_' + cfg.network.matching)
-    feature_out = osp.join(outputs_dir, f'feats-{cfg.network.detection}.h5')
-    deep_sfm_dir = osp.join(outputs_dir, 'sfm_ws')
-    model_path = osp.join(deep_sfm_dir, 'model')
+    outputs_dir = osp.join(outputs_dir_root, "outputs" + "_" + cfg.network.detection + "_" + cfg.network.matching)
+    feature_out = osp.join(outputs_dir, f"feats-{cfg.network.detection}.h5")
+    deep_sfm_dir = osp.join(outputs_dir, "sfm_ws")
+    model_path = osp.join(deep_sfm_dir, "model")
 
     # Select feature track length to limit the number of 3D points below the 'max_num_kp3d' threshold:
-    track_length, points_count_list = filter_tkl.get_tkl(model_path, thres=cfg.dataset.max_num_kp3d, show=False) 
-    filter_tkl.vis_tkl_filtered_pcds(model_path, points_count_list, track_length, outputs_dir) # For visualization only
+    track_length, points_count_list = filter_tkl.get_tkl(model_path, thres=cfg.dataset.max_num_kp3d, show=False)
+    filter_tkl.vis_tkl_filtered_pcds(model_path, points_count_list, track_length, outputs_dir)  # For visualization only
 
     # Leverage the selected feature track length threshold and 3D BBox to filter 3D points:
     xyzs, points_idxs = filter_points.filter_3d(model_path, track_length, bbox_path)
     # Merge 3d points by distance between points
-    merge_xyzs, merge_idxs = filter_points.merge(xyzs, points_idxs, dist_threshold=1e-3) 
+    merge_xyzs, merge_idxs = filter_points.merge(xyzs, points_idxs, dist_threshold=1e-3)
 
     # Save features of the filtered point cloud:
     feature_process.get_kpt_ann(cfg, img_lists, feature_out, outputs_dir, merge_idxs, merge_xyzs)
-    
 
-@hydra.main(config_path='cfg/', config_name='config.yaml')
+
+@hydra.main(config_path="cfg/", config_name="config.yaml")
 def main(cfg: DictConfig):
     globals()[cfg.type](cfg)
 
--- scripts/cad2rgbr.py
+++ scripts/cad2rgbr.py
@@ -6,29 +6,27 @@ import numpy as np
 import glob
 import os
 import cv2
-
+import tqdm
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
 def fibonacci_sphere_sampling(K, radius=1):
     indices = np.arange(0, K, dtype=float) + 0.5
-
     phi = np.arccos(1 - 2 * indices / K)
     theta = np.pi * (1 + 5**0.5) * indices
-
     x = np.cos(theta) * np.sin(phi)
     y = np.sin(theta) * np.sin(phi)
     z = np.cos(phi)
-
     points = np.array([x, y, z]).T
     return radius * points
 
 
 def generate_camera_poses(K, radius=1):
+    # fix the seed
+    np.random.seed(0)
     # Sample K points on the sphere
     points = fibonacci_sphere_sampling(K, radius)
-
     # Generate the camera poses
     camera_poses = []
     for point in points:
@@ -36,16 +34,13 @@ def generate_camera_poses(K, radius=1):
         x_axis = np.cross(z_axis, np.array([0, 1, 0]))
         x_axis /= np.linalg.norm(x_axis)
         y_axis = np.cross(z_axis, x_axis)
-
         rotation_matrix = np.stack([x_axis, y_axis, z_axis], axis=1)
         translation = -point
         # translation = np.array([0, 0, 0])
-
         camera_pose = np.eye(4)
         camera_pose[:3, :3] = rotation_matrix
         camera_pose[:3, 3] = translation
         camera_poses.append(camera_pose)
-
     return np.array(camera_poses)
 
 
@@ -61,27 +56,22 @@ def rotation_distance(pose1, pose2):
 def sort_camera_poses_by_rotation(camera_poses):
     sorted_poses = []
     unprocessed_poses = list(camera_poses)
-
     # Randomly pick the first camera pose
     first_pose_idx = np.random.randint(len(unprocessed_poses))
     current_pose = unprocessed_poses.pop(first_pose_idx)
     sorted_poses.append(current_pose)
-
     while unprocessed_poses:
         min_distance = float("inf")
         closest_pose_idx = None
-
         # Find the closest pose in rotation to the current one
         for i, pose in enumerate(unprocessed_poses):
             distance = rotation_distance(current_pose, pose)
             if distance < min_distance:
                 min_distance = distance
                 closest_pose_idx = i
-
         # Update the current pose and add it to the sorted list
         current_pose = unprocessed_poses.pop(closest_pose_idx)
         sorted_poses.append(current_pose)
-
     return np.array(sorted_poses)
 
 
@@ -92,51 +82,67 @@ def cad2video(cad_path, sfm_path):
     cad_model = o3d.io.read_triangle_mesh(cad_file, True)
     # scale the model by 1000 at origin
     cad_model.scale(1.0 / 1000.0, center=np.zeros(3, dtype=np.float64))
-
     init_pose = np.eye(4)
     # apply the init pose
     cad_model.transform(init_pose)
-
-    # visualize
-    # o3d.visualization.draw_geometries([cad_model])
-
     # prepare the sfm
-    output_dir = os.path.join(sfm_path, "images")
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
+    color_dir = os.path.join(sfm_path, "color")
+    poses_dir = os.path.join(sfm_path, "poses_ba")
+    intrin_dir = os.path.join(sfm_path, "intrin_ba")
+    os.makedirs(color_dir, exist_ok=True)
+    os.makedirs(poses_dir, exist_ok=True)
+    os.makedirs(intrin_dir, exist_ok=True)
+    # clean
+    for f in glob.glob(os.path.join(color_dir, "*.png")):
+        os.remove(f)
+    for f in glob.glob(os.path.join(poses_dir, "*.txt")):
+        os.remove(f)
+    for f in glob.glob(os.path.join(intrin_dir, "*.txt")):
+        os.remove(f)
     # change the pose and render
     K = 200  # Number of camera poses to generate
     radius = 0.2  # Radius of the sphere
     camera_poses = generate_camera_poses(K, radius)
     camera_poses = sort_camera_poses_by_rotation(camera_poses)
-
-    prev_pose = np.eye(4)
+    # set up the intrinsic camera parameters
+    width, height = 640, 480
+    focal = 500  # Focal lengths
+    cx, cy = width / 2 - 0.5, height / 2 - 0.5  # Principal point
+    intrinsic_params = o3d.camera.PinholeCameraIntrinsic(width, height, focal, focal, cx, cy)
+    camera_params = o3d.camera.PinholeCameraParameters()
+    camera_params.intrinsic = intrinsic_params
     pose_list = camera_poses
-    for i, pose in enumerate(pose_list):
-        # new_pose = np.linalg.inv(pose)
-        new_pose = pose
-        pose_delta = new_pose @ np.linalg.inv(prev_pose)
-        prev_pose = pose
-        cad_model.transform(pose_delta)
-        width, height = 400, 400
-
-        # Set the scene with the point cloud
+    for i, pose in tqdm.tqdm(enumerate(pose_list)):
+        new_pose = np.linalg.inv(pose)
+        # flip the z axis
+        new_pose[2, :] *= -1
+        camera_params.extrinsic = new_pose
+        # set the scene with the point cloud
         vis = o3d.visualization.Visualizer()
         vis.create_window(width=width, height=height, visible=False)
         vis.add_geometry(cad_model)
-        vis.update_renderer()
-        # Capture the image and close the visualizer
+        # visualize coordinate
+        frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
+        frame.transform(pose)
+        vis.add_geometry(frame)
+        vis.get_view_control().convert_from_pinhole_camera_parameters(camera_params)
+        # capture the image and close the visualizer
         image = vis.capture_screen_float_buffer(do_render=True)
         vis.destroy_window()
-
-        # Convert the image to a numpy array
+        # convert the image to a numpy array
         image = np.asarray(image)
-        # Convert the image to RGB
+        # convert the image to RGB
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # Save the images
-        image_name = os.path.join(output_dir, f"{i:04d}.png")
+        # save the images
+        image_name = os.path.join(color_dir, f"{i}.png")
         cv2.imwrite(image_name, image * 255)
+        # save the pose
+        output_pose = np.linalg.inv(pose)
+        output_pose[1, :] *= -1
+        output_pose[2, :] *= -1
+        np.savetxt(os.path.join(poses_dir, f"{i}.txt"), output_pose)
+        # save the intrinsic matrix
+        np.savetxt(os.path.join(intrin_dir, f"{i}.txt"), intrinsic_params.intrinsic_matrix)
 
 
 if __name__ == "__main__":
@@ -145,6 +151,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cad_path", type=str, default="data/RBOT_dataset/bakingsoda")
     parser.add_argument("--sfm_path", type=str, default="data/RBOT_dataset/bakingsoda/sfm")
-    parser.add_argument("--output_dir", type=str, default="data/sfm_model/bakingsoda")
+    parser.add_argument("--color_dir", type=str, default="data/sfm_model/bakingsoda")
     args = parser.parse_args()
-    cad2video(args.cad_path, args.output_dir)
+    cad2video(args.cad_path, args.color_dir)
